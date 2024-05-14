c0aa93eaea6e439ebe886448a6c35cbd0acbc214
crambelsoupy <cmorrison@elara.ai>
2024-05-14 10:43:05 +1000

    chore: adding working project

---
+++ src/renderer/src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { game } from "./game"
+import { Resources, loader } from './resources';
+const ipcHandle = () => window.electron.ipcRenderer.send('ping')
+game.start(loader).then(() => Resources.TiledMap.addToScene(game.currentScene));
+</script>
+
+<template>
+</template>
---
+++ src/renderer/src/game.ts
@@ -0,0 +1,10 @@
+import * as ex from 'excalibur';
+
+export const game = new ex.Engine({
+    width: 800,
+    height: 600,
+    canvasElementId: 'game',
+    pixelArt: true,
+    pixelRatio: 2
+});
+
